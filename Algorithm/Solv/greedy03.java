202d6956b922e23149692a22075725f8baa4074f
laegel <laegel1@gmail.com>
2022-01-05 01:03:42 +0900

    그리디 알고리즘 풀이

--- Algorithm/Solv/greedy03.java
+++ Algorithm/Solv/greedy03.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 /**
  * BOJ - 주식
@@ -13,7 +14,35 @@ public class greedy03 {
 
     public void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
         StringBuilder sb = new StringBuilder();
+
+        int t = Integer.parseInt(br.readLine());
+
+        int[] arr;
+        while (t-- > 0) {
+            int n = Integer.parseInt(br.readLine());
+            arr = new int[n];
+
+            // 배열 초기화
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(st.nextToken());
+            }
+
+            int total = 0;
+            int max = arr[n-1];
+            for (int i = n - 1; i >= 0; i--) {
+                if (arr[i] < max) {
+                    total += max-arr[i];
+                } else {
+                    max = arr[i];
+                }
+            }
+            sb.append(total).append("\n");
+        }
+
+        System.out.println(sb);
 
 
     }
