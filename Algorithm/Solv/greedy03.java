7a22e08cac31d8c1697b7e60214f68c6b825d2d5
laegel <laegel1@gmail.com>
2022-01-05 00:13:54 +0900

    그리디 알고리즘 풀이

---
+++ Algorithm/Solv/greedy03.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * BOJ - 주식
+ * https://www.acmicpc.net/problem/11501
+ *
+ * for문을 반대로 돌아 max 값 갱신하는 식으로 해결
+ *
+ */
+public class greedy03 {
+
+    public void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+    }
+}
