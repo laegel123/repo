57116d7720d104afead66575aa037f6bb9419218
laegel <laegel1@gmail.com>
2022-02-12 16:39:36 +0900

    그리디 문제풀이
    1. codeup : 거스름돈

--- Algorithm/Greedy/Solv/Greedy09.java
+++ Algorithm/Greedy/Solv/Greedy09.java
@@ -13,9 +13,22 @@ public class Greedy09 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        int[] coin = {50000, 10000, 5000, 1000, 500, 100, 10};
+        int[] coin = {50000, 10000, 5000, 1000, 500, 100, 50, 10};
+        int count = 0;
 
-        
+        int i = 0;
+        while (n > 0) {
+            if (n / coin[i] > 0) {
+                count += n / coin[i];
+                n = n % coin[i];
+            }
+            i++;
+            
+        }
+
+        System.out.println(count);
+
+
 
     }
 }
