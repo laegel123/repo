0c4fa6e53aacd0c264075a66cfbbc8f1ba0f5289
laegel123 <62921658+laegel123@users.noreply.github.com>
2022-02-17 14:17:25 +0900

    Update Greedy13.java

--- Algorithm/Greedy/Solv/Greedy13.java
+++ Algorithm/Greedy/Solv/Greedy13.java
@@ -8,28 +8,46 @@ import java.util.StringTokenizer;
  * BOJ : 주유소
  * https://www.acmicpc.net/problem/13305
  *
-**/
-public class Greedy13 {
+ **/
+public class Greedy14 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
 
         int n = Integer.parseInt(br.readLine());
-        int[] width = new int[n - 1];
-        int[] cost = new int[n];
+        long[] width = new long[n - 1];
+        long[] cost = new long[n];
 
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n-1; i++) {
-            width[i] = Integer.parseInt(st.nextToken());
+            width[i] = Long.parseLong(st.nextToken());
         }
 
         st = new StringTokenizer(br.readLine());
+        // 리터당 기름값 내림차순일 경우 저장
         for (int i = 0; i < n; i++) {
-            cost[i] = Integer.parseInt(st.nextToken());
+            long newCost = Long.parseLong(st.nextToken());
+            if (i == 0) {
+                cost[i] = newCost;
+            } else {
+                if (newCost > cost[i-1]) {
+                    cost[i] = cost[i-1];
+                } else {
+                    cost[i] = newCost;
+                }
+            }
         }
 
-        
+        long minCost = 0;
+
+        for (int i = 0; i < n-1; i++) {
+            minCost += cost[i] * width[i];
+        }
+
+        System.out.println(minCost);
+
+
 
 
 
